db3d6796de63cb37bc62e8edb11c0a7a8f014d5c
FatWalrus <youngsstanley@gmail.com>
2016-09-16 03:37:22 +0000

    add placeholders and regex validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :restaurant
+  
+  validates :rating, :comment, presence: true
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1, 
+    less_than_or_equal_to: 5,
+  }
 end
